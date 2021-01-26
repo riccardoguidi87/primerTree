78eac9d0fb506da8eb60cc05f0b6f1256b0ee053
riccardoguidi87 <66052903+riccardoguidi87@users.noreply.github.com>
2021-01-26 14:31:31 -0800

    updates

--- Riccardo/script.R
+++ Riccardo/script.R
@@ -1,2 +1,7 @@
 install.packages('primerTree')
+install.packages('directlabels')
+install.packages('gridExtra')
 
+library(primerTree)
+mammals_16S = search_primer_pair(name='Mammals 16S', 'CGGTTGGGGTGACCTCGGA', 'GCTGTTATCCCTAGGGTAACT')
+plot(mammals_16S)
